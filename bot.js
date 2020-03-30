448f3341f764e85ec4200993703a5a73e735ec16
naimabd <57469512+naimabd@users.noreply.github.com>
2020-03-30 19:44:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,25 +7,19 @@ client.on('ready', () => {
 });
 
 
-client.on('message', message => {
-if(message.content.startsWith(prefix + "sug")) {
-      message.delete()
- 
-const args = message.content.slice(prefix.length).trim().split(/ +/g);
- 
-  var suggestMessage = args.slice(1).join(" ")
-  if(!suggestMessage) return message.reply("الرجاء وضع اقتراح")
-  let suggestsEMBED = new Discord.RichEmbed()
-   .setColor('#0028db')
-   .setTitle(" new suggestions")
-   .setDescription(`**${suggestMessage}**`)
-   .setFooter(` suggestions : ${message.author.tag}`)
- 
-       let suggests = message.guild.channels.find(ch => ch.name === "suggestions");
-                   if (!suggests) return message.reply("Please create text chat : suggestions")
-               suggests.send(suggestsEMBED);
-}
-})
 
+
+client.on("message", message=>{
+if ((message.content.startsWith(prefix + "embed")) || (message.content.startsWith("امبد"))){
+  if(!message.member.hasPermission('ADMINISTRATOR')) return message.delete();
+  let args = message.content.split(" ").slice(1);
+    let say = new Discord.RichEmbed()
+    .setDescription(args.join("  "))
+    .setColor(0x23b2d6)
+    .setFooter(`EG SERVER `)
+   message.channel.sendEmbed(say);
+    message.delete();
+}
+  })
 
 client.login(process.env.TOKEN);
