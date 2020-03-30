bbf319cfc17bc0a0d4e5f6e885f825b25a168e77
naimabd <57469512+naimabd@users.noreply.github.com>
2020-03-30 17:03:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,40 +7,25 @@ client.on('ready', () => {
 });
 
 
-client.on('message', message => {let prefix = "ا";
-
-if(message.content.startsWith(prefix + "قترح")) {
-
+client.on('message', message => {let prefix = "-";
+if(message.content.startsWith(prefix + "sug")) {
       message.delete()
-  
+ 
 const args = message.content.slice(prefix.length).trim().split(/ +/g);
  
-var suggestMessage = args.slice(1).join(" ")
-
+  var suggestMessage = args.slice(1).join(" ")
   if(!suggestMessage) return message.reply("الرجاء وضع اقتراح")
-
   let suggestsEMBED = new Discord.RichEmbed()
-
    .setColor('#0028db')
-
    .setTitle(" new suggestions")
-
    .setDescription(`**${suggestMessage}**`)
-
    .setFooter(` suggestions : ${message.author.tag}`)
-
  
-
        let suggests = message.guild.channels.find(ch => ch.name === "suggestions");
-
                    if (!suggests) return message.reply("Please create text chat : suggestions")
-
                suggests.send(suggestsEMBED);
-
 }
-
 })
-
 
 
 client.login(process.env.TOKEN);
